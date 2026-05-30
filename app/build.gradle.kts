4168426bd5b6bcdb122bfb8a2984505ef5ac3df6
vfttffx57-arch <vfttffx57@gmail.com>
2026-05-30 19:24:20 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,7 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
-    }
+    
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
